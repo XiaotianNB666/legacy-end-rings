6e6db7ae7ae186dc77fc133dd9c58280bad279d7
XiaotianMC <BilibiliXiaotian@outlook.com>
2026-08-18 16:17:31 +0800

    fix: end island never generated in snapshot-9

--- src/main/java/cn/gcte/mixin/EndIslandFunctionMixin.java
+++ src/main/java/cn/gcte/mixin/EndIslandFunctionMixin.java
@@ -1,35 +1,16 @@
 package cn.gcte.mixin;
 
 import net.minecraft.util.Mth;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.levelgen.densityfunction.DensityFunction;
 import net.minecraft.world.level.levelgen.densityfunction.generator.EndIslandFunction;
-import net.minecraft.world.level.levelgen.LegacyRandomSource;
 import net.minecraft.world.level.levelgen.synth.SimplexNoise;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EndIslandFunction.class)
 public abstract class EndIslandFunctionMixin implements DensityFunction {
-	@Final
-    @Mutable
-	@Shadow
-	private SimplexNoise islandNoise;
-
-	@Inject(method = "<init>", at = @At("TAIL"))
-	private void overwriteIslandNoise(long seed, CallbackInfo ci) {
-		RandomSource islandRandom = new LegacyRandomSource(seed);
-		islandRandom.consumeCount(17292);
-		this.islandNoise = new SimplexNoise(islandRandom);
-	}
-
-
 	@Inject(method = "getHeightValue", at = @At("HEAD"), cancellable = true)
 	private static void injectOldOverflow(
             SimplexNoise islandNoise, int sectionX, int sectionZ, CallbackInfoReturnable<Float> cir
